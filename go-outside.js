9d8239e758edfd0bb65618ba4be192f43e62e3c5
Kelly James <kellyjamesclancy@gmail.com>
2016-08-31 18:56:07 +0000

    Adds conditionals exercises

---
+++ go-outside.js
@@ -0,0 +1,11 @@
+var temperature = 12;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleaves won't cut it!");
+} else {
+  console.log("Short sleaves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
